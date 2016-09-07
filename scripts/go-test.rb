547627227609de3abb9e5546c51eedccf917f676
Zhuochun <zhuochun@hotmail.com>
2016-09-07 23:59:11 +0800

    Colorize outputs :rainbow:

--- scripts/go-test.rb
+++ scripts/go-test.rb
@@ -1,21 +1,49 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-# Dependency: https://github.com/guard/listen - `gem install listen`
-#
-# Trigger in the directory, to listen on changes.
+# Run `go test` automatically after file changes
 #
 # alias wgo="~/dotfiles/scripts/go-test.rb"
 #
+# Dependency:
+#
+# - https://github.com/guard/listen
+#   gem install listen
+# - https://github.com/fazibear/colorize
+#   gem install colorize
+#
 require "logger"
 require "listen"
+require "colorize"
 
 LOG = Logger.new(STDOUT)
 LOG.level = Logger::INFO
+LOG.formatter = proc { |severity, datetime, progname, msg| "#{datetime}: #{msg}\n" }
 
 go_path_src = File.join(ENV["GOPATH"], "src", "")
+LOG.info("GOPATH: #{ENV["GOPATH"].bold}".blue)
 
-LOG.info("Listening #{Dir.pwd}")
+def go_test(relative_path, options = "")
+    output = IO.popen("go test #{relative_path}/... #{options}")
+    output_lines = output.readlines.map do |line|
+      case line
+      when /Error Trace:/
+        line.bold.cyan
+      when /Error:/
+        line.red
+      when /FAIL/
+        line.light_yellow.on_red
+      when /PASS:/
+        line.green
+      when /RUN/
+        line.light_cyan
+      else
+        line
+      end
+    end.join("")
+
+    LOG.info("\n#{output_lines}")
+end
 
 listener = Listen.to(Dir.pwd, only: /\.go$/, latency: 1) do |modified, added, removed|
   unique_paths = {}
@@ -25,14 +53,17 @@ listener = Listen.to(Dir.pwd, only: /\.go$/, latency: 1) do |modified, added, re
   end
 
   unique_paths.keys.each do |path|
-    LOG.info("Detected changes in #{path}")
+    relative_path = path.gsub(go_path_src, "")
+    LOG.info("Detected changes in #{relative_path.bold}".cyan)
 
-    output = IO.popen("go test #{path.gsub(go_path_src, "")}/... -v")
-
-    LOG.info("\n#{output.readlines.join("")}")
+    go_test(relative_path, "-v")
   end
 end
 
 listener.start
+
+LOG.info("Listening: #{Dir.pwd.bold}".blue)
+# Initial run on whole listen path
+go_test(Dir.pwd.gsub(go_path_src, ""))
 
 sleep # zZZZ
