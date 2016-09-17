398c0e4f65d706559b0e6523d0ef9555c7cf792c
Zhuochun <zhuochun@hotmail.com>
2016-09-18 02:22:16 +0800

    Improve highlight and add coverage tests 🍢

--- scripts/go-test.rb
+++ scripts/go-test.rb
@@ -23,8 +23,16 @@ LOG = Logger.new(STDOUT)
 LOG.level = Logger::INFO
 LOG.formatter = proc { |severity, datetime, progname, msg| "#{datetime}: #{msg}\n" }
 
-go_path_src = File.join(ENV["GOPATH"], "src", "")
+GO_PATH_SRC = File.join(ENV["GOPATH"], "src", "")
 LOG.info("GOPATH: #{ENV["GOPATH"].bold}".blue)
+
+PROJECT_ROOT = Dir.pwd.gsub(GO_PATH_SRC, "")
+LOG.info("Listening: #{PROJECT_ROOT.bold}".blue)
+
+# Record the last changed directory
+last_dir = ""
+# Cache all the packages
+cached_packages = {}
 
 def anybar_notify(color)
   any_bar = UDPSocket.new
@@ -33,59 +41,157 @@ def anybar_notify(color)
   any_bar.close
 end
 
-def go_test(relative_path, options = "")
-  anybar_notify("yellow")
-  output = IO.popen("go test #{relative_path}/... #{options}")
+def cache_packages(pkgs)
+  Dir.glob("**/*_test.go") do |file|
+    dir = File.dirname(file)
+    pkg = File.split(dir).last
 
-  succeed = true
-  output_lines = output.readlines.map do |line|
-    case line
-    when /Error Trace:/
-      line.bold.cyan
-    when /Error:/, /panic:/, /FAIL:/
-      line.red
-    when /false/ # build failed
-      line.light_yellow.on_red
-      succeed = false
-    when /^FAIL/ # the final FAIL
-      line.light_yellow.on_red
-      succeed = false
-    when /PASS:/
-      line.green
-    when /RUN/
-      line.light_cyan
-    else
-      line
-    end
-  end.join("")
-  anybar_notify(succeed ? "green" : "red")
-
-  LOG.info("\n#{output_lines}")
+    pkgs[pkg] = dir
+  end
 end
 
-listener = Listen.to(Dir.pwd, only: /\.go$/, latency: 1) do |modified, added, removed|
+def lookup_package(pkgs, name)
+  return nil if name.nil? || name.empty?
+
+  found = pkgs.find do |pkg, dir|
+    pkg == name || pkg.start_with?(name)
+  end
+
+  if found
+    LOG.info "Lookup package: #{name.bold}, found: #{found[1].to_s.bold}".cyan
+    found[1]
+  else
+    LOG.info "Lookup package: #{name.bold}, pkg not found".cyan
+    nil
+  end
+end
+
+def go_test(relative_path, options = "")
+  anybar_notify("yellow")
+
+  test_passed = true
+  output_lines = []
+  IO.popen("go test #{options} #{relative_path}/...") do |io|
+    while line = io.gets
+      # shorten and highlight path in line
+      shorten_regex = /#{Regexp.escape(PROJECT_ROOT)}([\w\/]*)/
+      line = line.gsub(shorten_regex, '.\1'.yellow)
+      # replace head indication
+      line = line.gsub(/^ok\b/, 'ok'.green.bold)
+      line = line.gsub(/^FAIL\b/, 'FAIL'.light_yellow.on_red.bold)
+      # colorize
+      line = case line
+             when /Error Trace:/, /Error:/, /panic:/, /FAIL:/
+               test_passed = false
+               line.red
+             when /RUN/
+               line.light_cyan
+             when /PASS:/
+               line.green
+             when /coverage: (\d+.\d)%/
+               word = "coverage: #{$1.to_s}%".bold
+               $1.to_f < 60.0 ? word.red.bold : word.green.bold
+               line.gsub(/coverage: (\d+.\d)%/, word)
+             else
+               line
+             end
+      # collect all the output lines
+      output_lines << line
+    end
+  end
+  LOG.info("\n#{output_lines.join}")
+
+  if $?.success? && test_passed
+    anybar_notify("green")
+  else
+    anybar_notify(test_passed ? "exclamation" : "red") # build fail, or test fail
+  end
+end
+
+listener = Listen.to(Dir.pwd, only: /\.go$/) do |modified, added, removed|
   unique_paths = {}
 
   [modified, added, removed].each do |group|
     group.each { |file| unique_paths[File.dirname(file)] = true }
   end
 
-  unique_paths.keys.each do |path|
-    relative_path = path.gsub(go_path_src, "")
-    LOG.info("Detected changes in #{relative_path.bold}".cyan)
+  print "\n" # perfectionism  ┬─┬ ノ( ^_^ノ )
 
+  unique_paths.keys.each do |path|
+    relative_path = path.gsub(GO_PATH_SRC, "")
+    LOG.info("Detected changes in #{relative_path.gsub(PROJECT_ROOT, '.').bold}".cyan)
+
+    last_dir = relative_path
     go_test(relative_path, "-v")
   end
+
+  print "> ".blue.bold
 end
 
 listener.start
 
-LOG.info("Listening: #{Dir.pwd.bold}".blue)
-# Initial run on whole listen path
-go_test(Dir.pwd.gsub(go_path_src, ""))
+# Listen on exit signal
+at_exit { anybar_notify("white") }
+# Cache the packages in project directory
+cache_packages(cached_packages)
+# Initial run on whole project
+go_test(PROJECT_ROOT)
 
-at_exit do
-  anybar_notify("white")
+# Listen on instructions
+loop do
+  print "> ".blue.bold
+
+  case $stdin.gets.chomp.strip.downcase
+  when /^test(\s+\w+)?$/
+    # run all tests in package or in project
+    input = $1 ? $1.strip : ""
+
+    if input.empty?
+      go_test(PROJECT_ROOT)
+    elsif pkg = lookup_package(cached_packages, input)
+      go_test(File.join(PROJECT_ROOT, pkg), "-v")
+    else
+      LOG.info "No package is identified"
+    end
+
+  when /^cov(\s+\w+)?$/
+    # run coverage tests of package
+    input = $1 ? $1.strip : ""
+
+    if input.empty?
+      go_test(last_dir, "-coverprofile=coverage.out") unless last_dir.empty?
+    elsif pkg = lookup_package(cached_packages, input)
+      go_test(File.join(PROJECT_ROOT, pkg), "-coverprofile=coverage.out")
+    else
+      LOG.info "No package is identified"
+    end
+
+  when 'covall', 'cov .'
+    # run coverage test of all packages
+    go_test(PROJECT_ROOT, "-cover")
+
+  when 'covreport', 'report'
+    # open coverage report
+    `go tool cover -html=coverage.out`
+
+  when 'refresh'
+    cached_packages = {}
+    cache_packages(cached_packages)
+    LOG.info "#{cached_packages.size} PACKAGES WITH TESTS:\n#{cached_packages.values.map(&:yellow).join("\n")}"
+
+  when 'quit', 'exit'
+    exit(0)
+
+  else
+    LOG.info <<-EOF
+    COMMANDS:
+
+    #{"test [package]".green.bold}: run all tests in the package or in the whole project
+    #{"cov [package]".green.bold}: run coverage test on the package or last triggered packages
+    #{"covall".green.bold}: run coverage tests for all packages
+    #{"covreport".green.bold}: open the coverage report in browser
+    #{"refresh".green.bold}: refresh the cached packages
+    #{"quit".green.bold}: exit script
+    EOF
+  end
 end
-
-sleep # zZZZ
