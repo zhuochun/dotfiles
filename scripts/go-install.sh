1dd92afe2ed097d4cbcf31186c12e722e1980b25
Zhuochun <zhuochun@hotmail.com>
2016-10-13 22:28:25 +0800

    Add go-install :goat:

---
+++ scripts/go-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install/update vim-go needed dependency
+# Somehow vim-go :GoInstallBinaries doesn't work for me
+# https://github.com/fatih/vim-go/blob/master/plugin/go.vim#L9
+
+go get -v -u github.com/nsf/gocode
+go get -v -u github.com/alecthomas/gometalinter
+go get -v -u golang.org/x/tools/cmd/goimports
+go get -v -u golang.org/x/tools/cmd/guru
+go get -v -u golang.org/x/tools/cmd/gorename
+go get -v -u github.com/golang/lint/golint
+go get -v -u github.com/rogpeppe/godef
+go get -v -u github.com/kisielk/errcheck
+go get -v -u github.com/jstemmer/gotags
+go get -v -u github.com/klauspost/asmfmt/cmd/asmfmt
+go get -v -u github.com/fatih/motion
+go get -v -u github.com/zmb3/gogetdoc
+go get -v -u github.com/josharian/impl
