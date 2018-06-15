d0f82852b02d1ffcf28f02508283cc0abefb2bb4
Zhuochun <zhuochun@hotmail.com>
2018-06-16 00:56:03 +0800

    Add Exclude Users and Alias 🐺

--- scripts/git-code-review.rb
+++ scripts/git-code-review.rb
@@ -21,6 +21,17 @@ require_relative './git-utils'
 #       "slack_hook": "...",
 #       "repo": "$GOPATH/src/...",
 #       "diff_url": "...",
+#       "time_window": {
+#           "days": [1, 2, 3, 4, 5],
+#           "hours": [10, 20]
+#       },
+#       "exclude_users": [],
+#       "alias_users": [
+#           {
+#               "name": "abc",
+#               "alias": "a"
+#           }
+#       ],
 #       "groups": [
 #           {
 #               "query": "phab-query-url",
@@ -49,7 +60,7 @@ def slack(hook, channel, msg)
   payload = {
     channel: channel,
     username: 'OhMyCodeReview',
-    icon_emoji: ':face_with_monocle:',
+    icon_emoji: [':face_with_monocle:', ':face_with_cowboy_hat:', ':merman:', ':wave:'].sample,
     text: msg
   }
 
@@ -104,18 +115,18 @@ def levenshtein_distance(s, t)
   n = t.length
   return m if n == 0
   return n if m == 0
-  d = Array.new(m+1) {Array.new(n+1)}
+  d = Array.new(m + 1) { Array.new(n + 1) }
 
-  (0..m).each {|i| d[i][0] = i}
-  (0..n).each {|j| d[0][j] = j}
+  (0..m).each { |i| d[i][0] = i }
+  (0..n).each { |j| d[0][j] = j }
   (1..n).each do |j|
     (1..m).each do |i|
-      d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
-                  d[i-1][j-1]       # no operation required
+      d[i][j] = if s[i - 1] == t[j - 1] # adjust index into string
+                  d[i - 1][j - 1]       # no operation required
                 else
-                  [ d[i-1][j]+1,    # deletion
-                    d[i][j-1]+1,    # insertion
-                    d[i-1][j-1]+1,  # substitution
+                  [d[i - 1][j] + 1,    # deletion
+                   d[i][j - 1] + 1,    # insertion
+                   d[i - 1][j - 1] + 1, # substitution
                   ].min
                 end
     end
@@ -125,43 +136,65 @@ end
 
 USERS = Hash.new { |hash, phid| hash[phid] = user_search(phid) }
 
+def list_reviewers(cfg, author, commit, paths)
+  # get reviewers who did review, ping them again
+  reviewers = commit.reviewers - commit.reviewed_by
+  reviewers = reviewers.reject { |r| r.start_with?('#', 'gocd') }
+  # no need to handle additional cases
+  return reviewers unless reviewers.empty?
+
+  # find previous committer to paths changed
+  committers = {}
+  paths.sample(9).each do |path|
+    git_blame(cfg.repo || Dir.pwd, path).each_pair do |name, v|
+      m = committers[name]
+
+      if m.nil?
+        m = v
+      else
+        m[:count] += v[:count]
+        m[:last_edit_at] = [v[:last_edit_at], m[:last_edit_at]].max
+      end
+
+      committers[name] = m
+    end
+  end
+  # handle exclude users
+  Array(cfg.exclude_users).each { |u| committers.delete(u) }
+  # handle alias
+  Array(cfg.alias_users).each do |u|
+    committers[u['alias']] = committers.delete(u['name']) if committers.key?(u['name'])
+  end
+  # handle duplicated self
+  committers.keys.each do |k|
+    name = k.tr('- ', '.').downcase
+    committers.delete(k) if levenshtein_distance(name, author.downcase) <= 5
+  end
+  # sort by changes
+  committers.sort_by { |_n, v| -v[:count] }.map { |n| n[0] }
+end
+
 def scan_diffs(cfg)
   cfg.groups.each do |group|
     revs = revision_search(group['query'])
+
     authors = []
     revs = revs.map do |rev|
       commit = commit_msg(rev.id)
       paths = commit_paths(rev.id)
 
-      reviewers = commit.reviewers - commit.reviewed_by
-      reviewers = reviewers.reject { |r| r.start_with?('#') }
-      if reviewers.empty?
-        modified_by = {}
-        paths.sample(9).each do |path|
-          git_blame(cfg.repo || Dir.pwd, path).each_pair do |name, v|
-            m = modified_by[name]
-
-            if m.nil?
-              m = v
-            else
-              m[:count] += v[:count]
-              m[:last_edit_at] = [v[:last_edit_at], m[:last_edit_at]].max
-            end
-
-            modified_by[name] = m
-          end
-        end
-        reviewers = modified_by.sort_by { |n, v| -v[:count] }.map { |n| n[0] }
-      end
-
       author = USERS[rev.fields['authorPHID']]
-      authors << author.fields['username']
+      author_name = author.fields['username']
 
-      OpenStruct.new({ revision: rev,
-                       author: author,
-                       commit: commit,
-                       paths: paths,
-                       reviewers: reviewers })
+      authors << author_name
+      reviewers = list_reviewers(cfg, author_name, commit, paths)
+
+      OpenStruct.new(revision: rev,
+                     author: author,
+                     author_name: author_name,
+                     commit: commit,
+                     paths: paths,
+                     reviewers: reviewers)
     end
 
     msgs = revs.map do |rev|
@@ -169,19 +202,13 @@ def scan_diffs(cfg)
       pp rev.author
       pp rev.reviewers
 
-      msg  = "*#{rev.author.fields['username']}* "
+      msg  = "*#{rev.author_name}* "
       msg += "<#{cfg.diff_url}#{rev.revision.id}|#{rev.revision.fields['title'].gsub(/[<>"'\\\/]/, '')}> "
 
       reviewer = rev.reviewers[0].to_s
-      # exclude self
-      if levenshtein_distance(reviewer.upcase, rev.author.fields['username'].upcase) <= 5
-        reviewer = rev.reviewers[1]
-      end
-      # random pick
-      if reviewer.nil? || reviewer.empty?
-        reviewer = (authors - [rev.author.fields['username']]).sample
-      end
-      # unless we still don’t have reviewer
+      # fallback to random pick another diff author
+      reviewer = (authors - [rev.author_name]).sample if reviewer.to_s.empty?
+      # skip if we still don’t have reviewer
       msg += "\n    Ping *#{reviewer}* " unless reviewer.nil?
 
       msg += "(_#{time_ago(rev.revision.fields['dateModified'])}_)"
@@ -190,37 +217,39 @@ def scan_diffs(cfg)
 
     if msgs.empty?
       msg = [
-        "Aiyoh, nothing to review. Make more diffs lah :diya:",
-        "Sial ah, everything has been reviewed :really_good_job:",
+        'Nothing to review. Aiyoh, make more diffs lah :diya:',
+        'Steady, everything has been reviewed :really_good_job:',
+        'Wah lau eh, bored is this bot, reminder there was nothing loh :bb8_run:',
+        'Ah boy/girl, diffs are empty, faster chope seats :chopsticks:'
       ].sample
 
-      slack(cfg.slack_hook, group['channel'], ":hourglass: *Code Review:*\n" + msg)
+      slack(cfg.slack_hook, group['channel'], msg)
     else
-      while !msgs.empty?
+      until msgs.empty?
         msg = ''
         msg += "\n" + msgs.shift.to_s while !msgs.empty? && (msg.length + msgs.first.to_s.length < 3000)
 
-        slack(cfg.slack_hook, group['channel'], ":hourglass: *Code Review:*" + msg)
+        slack(cfg.slack_hook, group['channel'], msg)
         sleep(1) # breath
       end
     end
   end
 end
 
-if ARGV.length != 1
-  abort "Usage: #{$PROGRAM_NAME} config.json"
-end
+abort "Usage: #{$PROGRAM_NAME} config.json" if ARGV.length != 1
 
 CONFIG_PATH = ARGV[0]
+cfg = load_config(CONFIG_PATH)
 
 loop do
-  now = Time.new()
+  now = Time.new
+  tw = cfg.time_window
 
-  if now.hour >= 10 && now.hour <= 20
+  if !tw || (tw['days'].include?(now.wday) && now.hour >= tw['hours'][0] && now.hour <= tw['hours'][1])
     begin
       cfg = load_config(CONFIG_PATH)
       scan_diffs(cfg)
-    rescue Exception => e
+    rescue StandardError => e
       pp e
     end
   end
